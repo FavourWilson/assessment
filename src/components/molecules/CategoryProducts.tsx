434469e559c721fba44c73addc9b5ba8bb0405f2
favour wilson <ebrusikefavour@gmail.com>
2023-05-05 18:26:30 +0100

    pages

---
+++ src/components/molecules/CategoryProducts.tsx
@@ -0,0 +1,10 @@
+
+const CategoryProducts = () => {
+  return (
+      <div className="hidden lg:flex lg:w-[150px] lg:justify-center  lg:border-2 lg:border-[#424242] bg-white lg:rounded-2xl">
+          <span>Converse Show</span>
+     </div>
+  )
+}
+
+export default CategoryProducts
